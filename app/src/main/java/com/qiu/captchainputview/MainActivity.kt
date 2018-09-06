abfffdc2c156768ba165177f52e7bb27afdf3260
qiurongc <qiurongcai@miguantech.com>
2018-09-06 11:54:19 +0800

    配置Bintray

--- app/src/main/java/com/qiu/captchainputview/MainActivity.kt
+++ app/src/main/java/com/qiu/captchainputview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.qiu.captchainputview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.qiu.captcha.CaptchaInputView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputView = findViewById<CaptchaInputView>(R.id.captchaView)
+
+        inputView.setOnInputCompleteListener {
+            Toast.makeText(this@MainActivity, "验证码：$it", Toast.LENGTH_LONG).show()
+        }
     }
 }
